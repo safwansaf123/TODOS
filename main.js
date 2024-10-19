b8b8af1c67297b69d5f3cc8030f952b65857e118
safwansaf123 <160263365+safwansaf123@users.noreply.github.com>
2024-10-20 01:56:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestions = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input", //only one input it will ask and take and write ,  for ask in yes os no just replace input to confirm but no input//
+            message: "what would you like to add in your todos?",
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm", // this will confirm yes or no, no input it will take //
+            message: "what would you like to add more in your todos?",
+            default: "true", //answer will be always yes//
+        },
+    ]);
+    todos.push(todoQuestions.firstQuestion); //to add data in array we use push//
+    console.log(todos);
+    condition = todoQuestions.secondQuestion;
+}
